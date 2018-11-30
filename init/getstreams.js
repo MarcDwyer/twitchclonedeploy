d3568a7bf9b4c6b6884fa2691596f400b09efa10
Marc Dwyer <envdia@gmail.com>
2018-11-30 12:55:55 -0600

    added cbs and higher interval

--- init/getstreams.js
+++ init/getstreams.js
@@ -5,28 +5,28 @@ const fs = require('fs');
 
     const API = 'AIzaSyBghJmzrFiYYr4ClicgFYHvN4ubVsnJxuE';
 
-    const streamList = [
-      {name:'Ice', channelId: 'UCv9Edl_WbtbPeURPtFDo-uA'},
-      {name:'Mixhound', channelId: 'UC_jxnWLGJ2eQK4en3UblKEw'},
-      {name:'Destiny', channelId: 'UC554eY5jNUfDq3yDOJYirOQ'},
-      {name:'Hyphonix', channelId: 'UC4abN4ZiybnsAXTkTBX7now'},
-      {name:'Marie', channelId: 'UC16fss-5fnGp2Drqp1iT9pA'},
-      {name:'Gary', channelId: 'UCvxSwu13u1wWyROPlCH-MZg'},
-      {name:'Burger', channelId: 'UC3MAdjjG3LMCG8CV-d7nEQA'},
-      {name:'Pepper', channelId: 'UCdSr4xliU8yDyS1aGnCUMTA'},
-      {name:'Evan', channelId: 'UCHYUiFsAJ-EDerAccSHIslw'},
-      {name:'Lolesports', channelId: 'UCvqRdlKsE5Q8mf8YXbdIJLw'},
-      {name:'Chilledcow', channelId: 'UCSJ4gkVC6NrvII8umztf0Ow'},
-      {name:'Cxnews', channelId: 'UCStEQ9BjMLjHTHLNA6cY9vg'},
-      {name: 'Andy', channelId: 'UCovb8rgpCANx6nwDwnW0Uqg'},
-      {name: 'Code', channelId: 'UCvjgXvBlbQiydffZU7m1_aw'},
-      {name: 'Joe', channelId: 'UCzQUP1qoWDoEbmsQxvdjxgQ'},
-      {name: 'Me', channelId: 'UCBawnZIFCiN_WNvsseGyjYA'},
-      {name: 'Nasa', channelId: 'UCLA_DiR1FfKNvjuUpBHmylQ'}
-    ];
+const streamList = [
+    {name:'Ice', channelId: 'UCv9Edl_WbtbPeURPtFDo-uA'},
+    {name:'Mixhound', channelId: 'UC_jxnWLGJ2eQK4en3UblKEw'},
+    {name:'Tsa', channelId: 'UCB0H_1M78_jwTyfaJuP241g'},
+    {name:'Destiny', channelId: 'UC554eY5jNUfDq3yDOJYirOQ'},
+    {name:'Hyphonix', channelId: 'UC4abN4ZiybnsAXTkTBX7now'},
+    {name:'Gary', channelId: 'UCvxSwu13u1wWyROPlCH-MZg'},
+    {name:'Burger', channelId: 'UC3MAdjjG3LMCG8CV-d7nEQA'},
+    {name:'Evan', channelId: 'UCHYUiFsAJ-EDerAccSHIslw'},
+    {name:'Lolesports', channelId: 'UCvqRdlKsE5Q8mf8YXbdIJLw'},
+    {name:'Chilledcow', channelId: 'UCSJ4gkVC6NrvII8umztf0Ow'},
+    {name:'Cxnews', channelId: 'UCStEQ9BjMLjHTHLNA6cY9vg'},
+    {name: 'Andy', channelId: 'UCovb8rgpCANx6nwDwnW0Uqg'},
+    {name: 'Code', channelId: 'UCvjgXvBlbQiydffZU7m1_aw'},
+    {name: 'Joe', channelId: 'UCzQUP1qoWDoEbmsQxvdjxgQ'},
+    {name: 'Me', channelId: 'UCBawnZIFCiN_WNvsseGyjYA'},
+    {name: 'Nasa', channelId: 'UCLA_DiR1FfKNvjuUpBHmylQ'},
+    {name: 'CBS', channelId: 'UC8p1vwvWtl6T73JiExfWs1g'}
+];
 
     giveList();
-    setInterval(giveList, 220000);
+    setInterval(giveList, 260000);
     async function giveList() {
         console.log('running...');
 try {
